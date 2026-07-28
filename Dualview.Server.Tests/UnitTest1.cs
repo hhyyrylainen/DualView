af66741be3875c882c264163021c9bbbde530cfe
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2026-07-28 11:03:56 +0300

    Initial template for the DualView 3 C# version

---
+++ Dualview.Server.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+﻿namespace Dualview.Server.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ Dualview.Shared/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace Dualview.Shared;
+
+public class Class1
+{
+}
