739438395dfbbc29c5082d2929b0ade171dfe9fb
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2026-08-13 19:41:46 +0300

    New collection media window behind cs file

--- DualView.GUI/Views/MediaCollectionWindow.axaml.cs
+++ DualView.GUI/Views/MediaCollectionWindow.axaml.cs
@@ -1,7 +1,5 @@
-using System;
 using DualView.GUI.ViewModels;
 using Avalonia.Controls;
-using Avalonia.Input.Platform;
 
 namespace DualView.GUI.Views;
 
@@ -10,49 +8,17 @@ public partial class MediaCollectionWindow : Window
     public MediaCollectionWindow()
     {
         InitializeComponent();
-
-        DataContextChanged += (s, e) =>
-        {
-            if (DataContext is MediaCollectionWindowViewModel vm)
-            {
-                vm.OnWantsToCreateFolder += OnNewFolder;
-
-                // Hook clipboard copy callback for folder path copying
-                vm.RequestCopyToClipboard = async text =>
-                {
-                    if (Clipboard == null)
-                        throw new InvalidOperationException("Clipboard is not available");
-
-                    await Clipboard.SetTextAsync(text);
-                };
-            }
-        };
+        DataContextChanged += OnDataContextChanged;
     }
 
-    private async void OnNewFolder(object? sender, EventArgs e)
+    private void OnDataContextChanged(object? sender, System.EventArgs e)
     {
-        if (DataContext is not MediaCollectionWindowViewModel vm)
-            return;
+        if (DataContext is MediaCollectionWindowViewModel viewModel)
+            viewModel.CloseRequested += OnCloseRequested;
+    }
 
-        var parentFolder = vm.LastSelectedFolderId;
-
-        var folderText = parentFolder == null
-            ? "It will be created in the root folder! "
-            : "It will be created in the last selected folder";
-
-        var dialogVm = new TextInputWindowViewModel(v => vm.TryCreateNewFolderAsync(v.Input, parentFolder, v))
-        {
-            Title = "New Media Folder",
-            ExplanationText = $"Enter a name for the new folder. {folderText}:",
-            InputPlaceholder = "Folder name...",
-        };
-
-        var dialog = new TextInputWindow
-        {
-            DataContext = dialogVm,
-            Title = dialogVm.Title,
-        };
-
-        await dialog.ShowDialog<bool>(this);
+    private void OnCloseRequested(object? sender, System.EventArgs e)
+    {
+        Close();
     }
 }
