ea7f39b059f6b205d4628db270ee7f1a5aae2e54
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2026-08-14 22:52:53 +0300

    Kind of reasonable first GUI for import section control

---
+++ DualView.GUI/Controls/ImportSectionControl.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace DualView.GUI.Controls;
+
+public partial class ImportSectionControl : UserControl
+{
+    public ImportSectionControl()
+    {
+        InitializeComponent();
+    }
+}
