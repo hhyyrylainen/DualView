5073f6a16a21e2ca5453d6742cb64baacaeb0cc8
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2026-08-15 11:54:08 +0300

    Added documentation to `IRealTimeDataHub` interface to clarify its intended usage and distinguish it from `ISignalRService`.

--- DualView.Shared/Services/IRealTimeDataHub.cs
+++ DualView.Shared/Services/IRealTimeDataHub.cs
@@ -1,5 +1,9 @@
 namespace DualView.Shared.Services;
 
+/// <summary>
+///   This is for bulk data that changes a lot. In contrast to <see cref="ISignalRService"/> which is for most other
+///   data that doesn't change that often. So most of the time any new signal should be added to that other interface!
+/// </summary>
 public interface IRealTimeDataHub
 {
     public Task OnChatMessageTextAppend(long chatId, int messageIndex, int messageGeneration, string newText);
