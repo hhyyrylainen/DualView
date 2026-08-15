bc5cf6672b10dab5a105fd8f52b09c00bbdb8334
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2026-08-15 21:37:27 +0300

    Implemented `ImportSectionBrowse` model to support collection browsing for upload sections.

---
+++ DualView.GUI/Models/ImportSectionBrowse.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Threading.Tasks;
+using DualView.Shared.Models.DTO;
+using DualView.Shared.Services;
+
+namespace DualView.GUI.Models;
+
+public sealed class ImportSectionBrowse(
+    long sectionId,
+    IClientDatabaseService databaseService,
+    IServiceProvider serviceProvider) : ICollectionBrowse
+{
+    public async Task<int> GetCountAsync()
+    {
+        return (await databaseService.GetUploadSectionAsync(sectionId))?.Media.Count ?? 0;
+    }
+
+    public async Task<int?> GetIndexAsync(long mediaId)
+    {
+        var media = (await databaseService.GetUploadSectionAsync(sectionId))?.Media;
+        if (media == null)
+            return null;
+
+        var index = media.FindIndex(item => item.Id == mediaId);
+        return index >= 0 ? index : null;
+    }
+
+    public async Task<IVisualMediaSource?> GetMediaAsync(int index)
+    {
+        var media = (await databaseService.GetUploadSectionAsync(sectionId))?.Media;
+        if (media == null || index < 0 || index >= media.Count)
+            return null;
+
+        return new ServerMediaSource(new ConfiguredMediaInfo(media[index]), serviceProvider);
+    }
+}
