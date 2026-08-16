2dd0f840657a80c64de5105098dcc3591353393b
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2026-08-16 19:07:01 +0300

    Tag Editor code behind

---
+++ DualView.GUI/Controls/TagEditor.axaml.cs
@@ -0,0 +1,25 @@
+using Avalonia.Controls;
+using Avalonia.Input;
+using DualView.GUI.ViewModels;
+
+namespace DualView.GUI.Controls;
+
+public partial class TagEditor : UserControl
+{
+    public TagEditor() => InitializeComponent();
+
+    private void OnTextChanged(object? sender, TextChangedEventArgs e)
+    {
+        if (DataContext is TagEditorViewModel viewModel && sender is AutoCompleteBox box)
+            _ = viewModel.LoadSuggestionsAsync(box.Text ?? string.Empty);
+    }
+
+    private void OnKeyDown(object? sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter && DataContext is TagEditorViewModel viewModel)
+        {
+            viewModel.AddTag();
+            e.Handled = true;
+        }
+    }
+}
