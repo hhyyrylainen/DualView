56f7c29a342c06ef84dd692f9c964f26d817b3e0
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2026-08-20 20:17:34 +0300

    Add `ExpandContent` functionality to `ImportSectionControl` for supporting expanded layouts

--- DualView.GUI/Controls/ImportSectionControl.axaml.cs
+++ DualView.GUI/Controls/ImportSectionControl.axaml.cs
@@ -1,13 +1,89 @@
+using System;
+using System.ComponentModel;
 using Avalonia.Controls;
+using Avalonia.Layout;
 using DualView.GUI.ViewModels;
 
 namespace DualView.GUI.Controls;
 
 public partial class ImportSectionControl : UserControl
 {
+    private bool expandContent;
+    private ImportSectionViewModel? dataContextViewModel;
+
     public ImportSectionControl()
     {
         InitializeComponent();
+        AttachedToVisualTree += (_, _) => ApplyExpandedLayout();
+        DataContextChanged += OnDataContextChanged;
+    }
+
+    /// <summary>
+    ///   When set, this takes up all the space it wants. So this is used when popped out to do a custom layout
+    ///  that is completely tall.
+    /// </summary>
+    public bool ExpandContent
+    {
+        get => expandContent;
+        set
+        {
+            if (expandContent == value)
+                return;
+
+            expandContent = value;
+            var verticalAlignment = value
+                ? VerticalAlignment.Stretch
+                : VerticalAlignment.Top;
+            CollectionLayout.VerticalAlignment = verticalAlignment;
+            ImagesLayout.VerticalAlignment = verticalAlignment;
+            ApplyImageSizing();
+
+            if (!value)
+                return;
+
+            ApplyExpandedLayout();
+        }
+    }
+
+    private void ApplyExpandedLayout()
+    {
+        if (!expandContent)
+            return;
+
+        CollectionLayout.VerticalAlignment = VerticalAlignment.Stretch;
+        ImagesLayout.VerticalAlignment = VerticalAlignment.Stretch;
+        ImageContentLayout.VerticalAlignment = VerticalAlignment.Stretch;
+        CollectionLayout.RowDefinitions[1].Height = new GridLength(1, GridUnitType.Star);
+        FolderPickerControl.ClearValue(HeightProperty);
+        FolderPickerControl.MinHeight = 305;
+        ApplyImageSizing();
+    }
+
+    private void OnDataContextChanged(object? sender, EventArgs e)
+    {
+        if (dataContextViewModel != null)
+            dataContextViewModel.PropertyChanged -= OnViewModelPropertyChanged;
+
+        dataContextViewModel = DataContext as ImportSectionViewModel;
+        if (dataContextViewModel != null)
+            dataContextViewModel.PropertyChanged += OnViewModelPropertyChanged;
+
+        ApplyImageSizing();
+    }
+
+    private void OnViewModelPropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(ImportSectionViewModel.PreviewScrollViewerHeight))
+            ApplyImageSizing();
+    }
+
+    private void ApplyImageSizing()
+    {
+        ImagesScrollViewer.Height = expandContent || dataContextViewModel == null
+            ? double.NaN
+            : dataContextViewModel.PreviewScrollViewerHeight;
+        ImagesScrollViewer.MinHeight = 0;
+        ImagesScrollViewer.VerticalAlignment = VerticalAlignment.Stretch;
     }
 
     private void OnTargetNameTextChanged(object? sender, TextChangedEventArgs e)
