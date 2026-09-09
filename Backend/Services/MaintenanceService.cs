231c4b6c24feea9dae57c13812371729141fd615
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2026-09-10 00:45:24 +0300

    Extend temporary media retention period to 96 hours

--- Backend/Services/MaintenanceService.cs
+++ Backend/Services/MaintenanceService.cs
@@ -12,7 +12,7 @@ public class MaintenanceService : IMaintenanceService
     private static readonly TimeSpan OldProcessedFileTime = TimeSpan.FromDays(60);
     private static readonly TimeSpan DeletedImageTime = TimeSpan.FromDays(60);
     private static readonly TimeSpan DeletedCollectionTime = TimeSpan.FromHours(48);
-    private static readonly TimeSpan TemporaryMediaTime = TimeSpan.FromHours(72);
+    private static readonly TimeSpan TemporaryMediaTime = TimeSpan.FromHours(96);
     private static readonly TimeSpan RemoteMediaTime = TimeSpan.FromDays(30);
 
     private readonly ILogger<MaintenanceService> logger;
